d74555f02022ce845c02e570ee5e9193c9472d9b
Andreas.Frank <afrank.mmt-m2014@fh-salzburg.ac.at>
2014-10-26 12:30:46 +0100

    (swipe and Start-/Stop-Button works, some other adaptions

--- www/js/controllers/menu.js
+++ www/js/controllers/menu.js
@@ -1,6 +1,7 @@
 angular.module('respiratoryFrequency').controller('MenuController', function ($scope) {
   this.tab = 1;
 
+  // Methods for the menu-buttons
   this.selectTab = function(setTab) {
     this.tab = setTab;
   };
@@ -9,6 +10,7 @@ angular.module('respiratoryFrequency').controller('MenuController', function ($s
     return this.tab === checkTab;
   };
 
+  // Methods for the swipe-function
   this.goToLiveView = function() {
     this.selectTab(1);
   };
